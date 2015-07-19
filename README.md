127ac7422d3582d691da936dd07213fa6dcfb533
eliran zach <thehobbit85@gmail.com>
2015-07-19 15:20:47 +0300

    Added travis and readme

---
+++ README.md
@@ -0,0 +1,2 @@
+# Colu-Node.Js
+[![Build Status](https://travis-ci.org/Colu-platform/colu-nodejs.svg?branch=master)](https://travis-ci.org/Colu-platform/colu-nodejs) [![Coverage Status](https://coveralls.io/repos/Colu-platform/colu-nodejs/badge.svg?branch=master)](https://coveralls.io/r/Colu-platform/colu-nodejs?branch=master) [![npm version](https://badge.fury.io/js/colu-nodejs.svg)](http://badge.fury.io/js/colu-nodejs)
--- src/colu.js
+++ src/colu.js
@@ -10,40 +10,40 @@ var coluHost = 'https://engine.colu.co'
 var FEE = 1000
 
 Colu = function (args) {
-	var self = this
+  var self = this
 
-	self.coluHost = args.coluHost || coluHost
+  self.coluHost = args.coluHost || coluHost
 
-	self.hdwallet = new HDWallet(args)
-	self.cc = new CC(args)
+  self.hdwallet = new HDWallet(args)
+  self.cc = new CC(args)
 
-	self.hdwallet.on('connect', function () {
-		self.emit('connect')
-	})
+  self.hdwallet.on('connect', function () {
+    self.emit('connect')
+  })
 
-	self.hdwallet.on('error', function (err) {
-		self.emit('error', err)
-	}) 
+  self.hdwallet.on('error', function (err) {
+    self.emit('error', err)
+  })
 }
 
 util.inherits(Colu, events.EventEmitter)
 
 Colu.prototype.init = function () {
-	var self = this
+  var self = this
 
-	self.hdwallet.init()
+  self.hdwallet.init()
 }
 
 Colu.prototype.financedIssue = function (args, callback) {
-	var self = this
+  var self = this
 
-	var privateKey = self.hdwallet.getPrivateKey(args.accountIndex)
-	var publicKey = privateKey.pub
-	var last_txid
-	var assetInfo
-	args.fee = args.fee || FEE
+  var privateKey = self.hdwallet.getPrivateKey(args.accountIndex)
+  var publicKey = privateKey.pub
+  var last_txid
+  var assetInfo
+  args.fee = args.fee || FEE
 
-	async.waterfall([
+  async.waterfall([
     // Ask for finance.
     function (cb) {
       var data_params = {
@@ -61,19 +61,19 @@ Colu.prototype.financedIssue = function (args, callback) {
       }
       body = JSON.parse(body)
       last_txid = body.txid
-      
+
       args.financeOutputTxid = last_txid
       args.financeOutput = body.vout
       args.issueAddress = publicKey.getAddress(self.hdwallet.network).toString()
 
       if (args.transfer) {
-      	args.transfer.forEach(function (to) {
-      		if (!to.address) {
-      			to.address = publicKey.getAddress(self.hdwallet.network).toString()
-      		}
-      	})
+        args.transfer.forEach(function (to) {
+          if (!to.address) {
+            to.address = publicKey.getAddress(self.hdwallet.network).toString()
+          }
+        })
       }
-      
+
       return self.cc.issue(args, cb)
     },
     function (l_assetInfo, cb) {
@@ -102,22 +102,22 @@ Colu.prototype.financedIssue = function (args, callback) {
 }
 
 Colu.prototype.financedSend = function (args, callback) {
-	var self = this
+  var self = this
 
-	var privateKey
-	var publicKey
-	var last_txid
-	var sendInfo
-	args.fee = args.fee || FEE
+  var privateKey
+  var publicKey
+  var last_txid
+  var sendInfo
+  args.fee = args.fee || FEE
 
-	async.waterfall([
-		function (cb) {
-			self.hdwallet.getAddressPrivateKey(args.from, cb)
-		},
+  async.waterfall([
+    function (cb) {
+      self.hdwallet.getAddressPrivateKey(args.from, cb)
+    },
     // Ask for finance.
     function (priv, cb) {
-    	privateKey = priv
-    	publicKey = privateKey.pub
+      privateKey = priv
+      publicKey = privateKey.pub
       var data_params = {
         company_public_key: publicKey.toHex(),
         purpose: 'Send',
@@ -133,11 +133,8 @@ Colu.prototype.financedSend = function (args, callback) {
       }
       body = JSON.parse(body)
       last_txid = body.txid
-      
       args.financeOutputTxid = last_txid
       args.financeOutput = body.vout
-
-      
       return self.cc.sendasset(args, cb)
     },
     function (l_sendInfo, cb) {
