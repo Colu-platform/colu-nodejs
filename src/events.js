10761d4332ecb8718b556b59c358c50ea7ea4dc4
bejavu <bejavu@gmail.com>
2015-10-06 17:53:08 +0300

    events, and local storage bug fixed

---
+++ src/events.js
@@ -0,0 +1,25 @@
+var util = require('util')
+var events = require('events')
+var io = require('socket.io-client')
+
+function Events (args) {
+	var self = this
+	
+	args = args || {}
+	self.host = args.host || ''
+	self.rooms = args.rooms || []
+	self.socket = io.connect(host+'/events')
+	self.socket.on('connect', function () {
+		self.rooms.forEach(function (room) {
+			self.socket.emit('join', room)
+		})
+	})
+
+	self.rooms.forEach(function (room) {
+		self.socket.on(room, function (data) {
+			self.emit(room, data)
+		})
+	})
+}
+
+util.inherits(Events, events.EventEmitter)
--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-var Colu = require(__dirname + '/../colu.js')
+var Colu = require(__dirname + '/../src/colu.js')
 var assert = require('assert')
 var expect = require('chai').expect
 
